5a7cd49929e94b0ed28e86cb7f675a71ea58b45e
RoninNapoletano <74654834+RoninNapoletano@users.noreply.github.com>
2022-11-13 12:08:01 -0500

    Fix rest assured issued

--- testapi/src/main/java/org/testapi/Main.java
+++ testapi/src/main/java/org/testapi/Main.java
@@ -1,7 +1,19 @@
 package org.testapi;
 
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
 public class Main {
     public static void main(String[] args) {
+        RestAssured.baseURI = "https://reqres.in/";
+        RequestSpecification httpRequest = RestAssured.given();
 
+        Response response = httpRequest.request(Method.GET, "api/users?page=2");
+
+        int statusCode = response.getStatusCode();
+        System.out.println("La réponse ets ");
     }
 }
