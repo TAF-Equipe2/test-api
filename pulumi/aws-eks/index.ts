bf03185a43bfb2335ce2659001b55b89e2440ec6
Maxime Durand <durandmaxime1931@gmail.com>
2022-11-22 17:40:13 -0500

    Updated instance type for eks nodes to 't4g.small' (2 nodes).

--- pulumi/aws-eks/index.ts
+++ pulumi/aws-eks/index.ts
@@ -11,7 +11,7 @@ const config = new pulumi.Config();
 const minClusterSize = config.getNumber("minClusterSize") || 1;
 const maxClusterSize = config.getNumber("maxClusterSize") || 2;
 const desiredClusterSize = config.getNumber("desiredClusterSize") || 2;
-const eksNodeInstanceType = config.get("eksNodeInstanceType") || "t4g.small";
+const eksNodeInstanceType = config.get("eksNodeInstanceType") || "t3a.small";
 const vpcNetworkCidr = config.get("vpcNetworkCidr") || "10.0.0.0/16";
 
 // Create a new VPC
