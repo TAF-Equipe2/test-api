6e202bb42d85cbb055ead586ff890907d9c00393
RoninNapoletano <74654834+RoninNapoletano@users.noreply.github.com>
2022-11-22 23:31:47 -0500

    Getter Setter testApiPayload

--- testapi/src/main/java/org/requests/payload/request/TestApiRequest.java
+++ testapi/src/main/java/org/requests/payload/request/TestApiRequest.java
@@ -7,7 +7,20 @@ public class TestApiRequest {
     private String exceptedOutput;
     private String input;
 
+    public String getApiUrl() { return apiUrl; }
+    public void setApiUrl(String apiUrl) { this.apiUrl = apiUrl; }
 
+    public String getMethod() { return method; }
+
+    public void setMethod(String method) { this.method = method; }
+
+    public String getExceptedOutput() { return exceptedOutput; }
+
+    public void setExceptedOutput(String exceptedOutput) { this.exceptedOutput = exceptedOutput; }
+
+    public String getInput() { return input; }
+
+    public void setInput(String input) { this.input = input; }
 }
 
 
