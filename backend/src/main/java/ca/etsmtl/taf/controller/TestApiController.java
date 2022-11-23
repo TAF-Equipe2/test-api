4af3bdeb38af2ca6056166e83cf8601066f69e7e
RoninNapoletano <74654834+RoninNapoletano@users.noreply.github.com>
2022-11-23 11:39:04 -0500

    Complete communication frontend - backend - microservice

--- backend/src/main/java/ca/etsmtl/taf/controller/TestApiController.java
+++ backend/src/main/java/ca/etsmtl/taf/controller/TestApiController.java
@@ -1,15 +1,14 @@
 package ca.etsmtl.taf.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 import ca.etsmtl.taf.payload.request.TestApiRequest;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse.BodyHandlers;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -18,13 +17,15 @@ public class TestApiController {
     @PostMapping("/checkApi")
     public void testApi(@Valid @RequestBody TestApiRequest testApiRequest) throws URISyntaxException, IOException, InterruptedException {
 
-        var client = HttpClient.newHttpClient();
-        var uri = new URI("http://localhost:8082/api/microservice/checkApi");
+        var uri = new URI("http://localhost:8082/microservice/testapi/checkApi");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(testApiRequest);
 
-        var request = HttpRequest.newBuilder(uri).
-                POST(BodyPublishers.ofString(testApiRequest.toString()))
-                .header("Content-type", "application/json").
-                build();
-        //Later add return info from microservice testapi
+        HttpRequest request = HttpRequest.newBuilder(uri)
+                .header("Content-Type", "application/json")
+                .POST(BodyPublishers.ofString(requestBody))
+                .build();
     }
 }
