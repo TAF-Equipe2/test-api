ab3154d1ea41dd1f4a00317926c7f6bb58be4f43
Polo <pablo_mo@orange.fr>
2022-11-24 10:08:26 -0500

    Deploy ingress

--- pulumi/aws-eks/index.ts
+++ pulumi/aws-eks/index.ts
@@ -1,6 +1,8 @@
+import { url } from "./ingress";
 import { back_url, front_url } from "./taf";
 import { team_1_url } from "./team-1";
 
 export const taf_back_url = back_url;
 export const taf_front_url = front_url;
 export const team_1_ui_url = team_1_url;
+export const global_url = url;
