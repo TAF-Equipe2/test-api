d6bcbfc19157f77a2fe4314fdce656b9f034304e
Polo <pablo_mo@orange.fr>
2022-11-24 09:07:47 -0500

    Deploy team 1

--- pulumi/aws-eks/index.ts
+++ pulumi/aws-eks/index.ts
@@ -1,14 +1,6 @@
 import { back_url, front_url } from "./taf";
-
-
-
-
-
-// const repo_tests_ui = new awsx.ecr.Repository("tests-ui");
-// const image_tests_ui = repo_tests_ui.buildAndPushImage("../../tests-ui/tests-ui");
-// const repo_selenium = new awsx.ecr.Repository("selenium");
-// const image_selenium = repo_selenium.buildAndPushImage("../../selenium");
-
+import { team_1_url } from "./team-1";
 
 export const taf_back_url = back_url;
 export const taf_front_url = front_url;
+export const team_1_ui_url = team_1_url;
