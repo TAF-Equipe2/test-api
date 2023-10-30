7a7409c7d4eb8b60c1373d44eaffad67aa2eb285
bobopelloye <bobopelloye@gmail.com>
2023-10-30 16:44:30 -0400

    mise a jour de specification du header

--- testapi/src/main/java/org/requests/TestApiController.java
+++ testapi/src/main/java/org/requests/TestApiController.java
@@ -7,28 +7,60 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.io.Serializable;
 
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import java.util.Map;
+import org.requests.Method;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/microservice/testapi")
 public class TestApiController {
-    private final SpecificationHeaderController headerTestController;
-    
-    public TestApiController(SpecificationHeaderController headerTestController) {
-        this.headerTestController = headerTestController;
-    }
 
     @PostMapping("/checkApi")
     public Serializable testApi(@Valid @RequestBody TestApiRequest testApiRequest) {
-        return (redirectMethod(testApiRequest));
-    }
 
-    @PostMapping("/checkHeaders")
-    public Map<String, Object> checkHeaders(@Valid @RequestBody TestApiRequest testApiRequest) {
-        return headerTestController.testHeaders(testApiRequest.getApiUrl(), testApiRequest.getExpectedHeaders());
+        Method method = testApiRequest.getMethod();
+        String apiUrl = testApiRequest.getApiUrl();
+        Map<String, String> expectedHeaders = testApiRequest.getExpectedHeaders();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        for (Map.Entry<String, String> entry : expectedHeaders.entrySet()) {
+            headers.set(entry.getKey(), entry.getValue());
+        }
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+        String methodAsString = convertMethodToString(method);
+
+        String response = restTemplate.exchange(apiUrl,  HttpMethod.resolve(methodAsString), entity, String.class).getBody();
+
+        System.out.println(response);
+
+        return (redirectMethod(testApiRequest));
     }
 
     public Serializable redirectMethod(TestApiRequest request) {
         return new RequestController(request).getAnswer();
+    }
+
+    private String convertMethodToString(Method method) {
+        switch (method) {
+            case GET:
+                return "GET";
+            case POST:
+                return "POST";
+            case PUT:
+                return "PUT";
+            case DELETE:
+                return "DELETE";
+            default:
+                return "GET"; 
+        }
     }
 
 
